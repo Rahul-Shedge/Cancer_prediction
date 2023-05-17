c007f0582609bb885ebea4a4636d669cf248754c
Rahul-Shedge <rahulshedge555@outlook.com>
2023-05-18 00:52:53 +0530

    code for preproccessing on data

---
+++ src/stage_01_preprocesing.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import yaml
+import os
+import argparse
+import logging
+from utils.common import read_csv,read_yaml,creat_dir
+from sklearn.preprocessing import StandardScaler
+from utils.model_utils import save_binary
+from sklearn.model_selection import train_test_split
+
+
+creat_dir(["logs"])
+
+logging.basicConfig(
+    filename=os.path.join("logs","running_logs.log"),
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+)
+
+
+
+
+
+def main(config_path):
+
+    try:
+        configs = read_yaml(config_path)
+        path = os.path.join(configs['SOURCE_DATA_DIRS']["DIR"],configs['SOURCE_DATA_DIRS']["FILENAME"])  
+        logging.info(f"Read input data path from configs file :{path}")
+        #print("path:",path)
+        data = read_csv(path)
+        logging.info(f"Data from {path} read successfully.")
+        #print(data.head())
+
+        y = data.diagnosis
+        X = data.drop(["id","diagnosis","Unnamed: 32"],axis=1)
+
+        to_drop = configs["PARAMS"]["COLS_TO_DROP"]
+        logging.info(f"Cols drop from input data {to_drop}")
+        filtered_X = X.drop(to_drop,axis=1)
+
+
+
+        X_train,X_test,y_train,y_test = train_test_split(filtered_X,y,train_size=configs["PARAMS"]["TRAIN_SPLIT_SIZE"])
+        ARTIFACTS = configs["ARTIFACTS"]
+
+        sc1 = StandardScaler()
+
+        sc1.fit(X_train)
+        artifacts_path = ARTIFACTS["ARTIFACTS_PATH"]
+        creat_dir([artifacts_path])
+        scaler_path = os.path.join(artifacts_path,ARTIFACTS["SCALER_BIN"])
+        save_binary(sc1, scaler_path)
+        
+        train_path = os.path.join(artifacts_path,ARTIFACTS["TRAIN_LOADER_BIN"])
+        test_path = os.path.join(artifacts_path,ARTIFACTS["TEST_LOADER_BIN"])
+        
+        X_train_f = sc1.transform(X_train)
+        X_test_f = sc1.transform(X_test)
+
+        save_binary(X_train_f, train_path)
+        save_binary(X_test_f,test_path)
+        return 
+        
+    except Exception as e:
+        print(e)
+
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="configs/configs.yaml")
+    parsed_args = args.parse_args()
+    main(config_path=parsed_args.config)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
