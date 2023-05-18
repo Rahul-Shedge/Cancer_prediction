0d0e701ac93b86a8be57b04044e7583511f26d99
Rahul-Shedge <rahulshedge555@outlook.com>
2023-05-18 19:37:49 +0530

    setup.py  created

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "Cancer_prediction"
+AUTHOR_USER_NAME = "Rahul-Shedge"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = []
+
+setup (
+    name = SRC_REPO,
+    version= "0.0.1",
+    author= AUTHOR_USER_NAME,
+    description= "MLOps workflow Cancer prediction",
+    long_description= long_description,
+    long_description_content_type = "test/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email= "rahulshedge555@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    py_modules= "=3.8.0",
+    libraries=LIST_OF_REQUIREMENTS
+)
+
+
+
+
+
