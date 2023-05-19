1b5f6011aa629b81076b28cf715220bce762e9e9
Rahul-Shedge <rahulshedge555@outlook.com>
2023-05-19 20:18:10 +0530

    experiment id added

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import mlflow
 
 
 def main():
-    with mlflow.start_run(run_name="main") as run:
+    exp_id = mlflow.set_experiment("Trail_run_1") # provide same experiment name in CLI with "--experiment-name Trail_run_1" do not use quotes
+    with mlflow.start_run(run_name="main__",experiment_id=exp_id.experiment_id) as run:
         mlflow.run(".","prep",env_manager="local")
         mlflow.run(".","train",env_manager="local")
         mlflow.run(".","test",env_manager="local")
